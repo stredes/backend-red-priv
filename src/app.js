d823361d16ce87990d15d286436caea850c46d87
stredes <gianlucassanmartin@gmail.com>
2026-01-04 13:54:38 -0300

    04012026....

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ const corsOptions = {
   credentials: true
 };
 app.use(cors(corsOptions));
-app.options('/*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
